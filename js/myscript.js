a8b3b318bdb677cff188092782199a4f558f952d
Matteo Ferrarese <matteo.ferrarese@yahoo.it>
2020-11-20 16:30:37 +0100

    Struttura1

---
+++ js/myscript.js
@@ -0,0 +1,20 @@
+// Questa consisterà nel creare la struttura html e il layout completi,
+// che saranno pensati su ciò che abbiam visto.NOTE
+//
+//     ricordatevi di andare in modo ordinato e lineare;
+//
+//     scrivere codice pulito e mantenibile vi salverà dal dover impazzire successivamente;
+//
+//     ricordatevi che qui siamo su layout App e non page;
+//
+//     se non vi ricordate qualcosa su ciò che abbiam visto dell’App avete sempre l’ultima parte di video di oggi (appena lo carico);
+//
+//     quindi date una bella occhiata quando avete finito e fate del refactoring ovunque possa essere utile;
+//
+//     se proprio finite e avete ancora del tempo potete iniziare a pensare a che struttura dati potrebbe aver senso fare;
+//
+//     se anche qui per caso volete proprio iniziare ad imbastire il milestone successivo, fatelo in una sotto cartella del repo;
+//
+//     la root/cartella principale del repo avrà sempre e solo il milestone assegnato quel giorno;
+//
+//     ricordate anche di riposare, al massimo i dettagli li posso sempre aggiustare strada facendo.
